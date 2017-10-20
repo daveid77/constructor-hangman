a52a58e290231919edd790bad831a44194d0c393
david morse <davidbryanmarshall@gmail.com>
2017-10-19 22:39:49 -0700

    inquirer validation for letter set

--- game.js
+++ game.js
@@ -2,29 +2,32 @@ var inquirer = require('inquirer');
 var word = require('./word');
 var letter = require('./letter');
 
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+console.log(alphabet.join('')); 
 
+var question = [
+  {
+    name: 'letter',
+    message: 'Guess a letter:',
+    type: 'input',
+    filter: function(answer) {
+      return answer.toLowerCase();
+    },
+    validate: function(answer) {
+      var alphaIncl = alphabet.includes(answer);
+      if (answer.length !== 1) {
+        return 'Please, enter a single letter.';
+      } else if (!alphaIncl) {
+        return 'Please, enter a letter.';
+      }
+      return true;
+    }
+  }
+];
 
-word.printInfo();
-
-
-
-// var questions = [
-//   {
-//   name: 'letter',
-//   message: 'Guess a letter:',
-//   type: 'input',
-//   filter: function(answers) {
-//     return answers.toLowerCase();
-//   },
-//   validate: function(answers) {
-//     return answers !== 'yoda'; //Yoda had *better not* use this application!!
-//   }
-// }
-// ];
-
-// inquirer.prompt(questions).then(function(answers) {
-//   console.log('\x1b[32m%s\x1b[0m', 'CORRECT!!');
-//   console.log('\x1b[31m%s\x1b[0m', 'Wrong. Try again.');
-//   //console.log(JSON.stringify(answers, null, '  '));
-// });
+inquirer.prompt(question).then(function(answer) {
+  console.log('\x1b[32m%s\x1b[0m', 'CORRECT!!');
+  console.log('\x1b[31m%s\x1b[0m', 'Wrong. Try again.');
+  //console.log(JSON.stringify(answer, null, '  '));
+});
