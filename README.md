cc16f04e1a210eaaaff05d202552056399269c1a
david morse <davidbryanmarshall@gmail.com>
2017-10-19 20:53:39 -0700

    fill in words/phrases and update README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# constructor-hangman
+# Constructor Hangman
+
+Week Six Homework Part 1: [Advanced Javascript Constructor Assignment](http://ucb.bootcampcontent.com/UCB-Coding-Bootcamp/09-11-2017-UCB-Class-Repository-FSF-FT/blob/master/06-week/homework/part-1/HomeworkInstructions.md).
+
+## Installation
+
+```
+git clone https://github.com/dbmarshall/constructor-hangman.git
+cd constructor-hangman
+npm install
+```
+
+## Available Node Commands
+
+* `node game.js`
+
+## Misc Notes
+
+* Letter validation built in.  Won't accept integers or multi-character strings. 
+
+## Author
+
+* **David Morse** ([dbmarshall.github.io](https://dbmarshall.github.io))
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
--- game.js
+++ game.js
@@ -1,14 +1,30 @@
-var inquirer = require("inquirer");
-
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "Guess a letter"
-  }
-]).then(function(user) {
+var inquirer = require('inquirer');
+var word = require('./word');
+var letter = require('./letter');
 
 
 
-  }
-});
+
+word.printInfo();
+
+
+
+// var questions = [
+//   {
+//   name: 'letter',
+//   message: 'Guess a letter:',
+//   type: 'input',
+//   filter: function(answers) {
+//     return answers.toLowerCase();
+//   },
+//   validate: function(answers) {
+//     return answers !== 'yoda'; //Yoda had *better not* use this application!!
+//   }
+// }
+// ];
+
+// inquirer.prompt(questions).then(function(answers) {
+//   console.log('\x1b[32m%s\x1b[0m', 'CORRECT!!');
+//   console.log('\x1b[31m%s\x1b[0m', 'Wrong. Try again.');
+//   //console.log(JSON.stringify(answers, null, '  '));
+// });
