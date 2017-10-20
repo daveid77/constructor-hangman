163401d7c53b86c59642f8065ef4a64f73eb7cfd
david morse <davidbryanmarshall@gmail.com>
2017-10-19 22:45:51 -0700

    adding word/phrase JS

---
+++ word.js
@@ -0,0 +1,33 @@
+console.log('word.js');
+
+var phrases = [
+    'Grandmaster Flash and The Furious Five', 
+    'Afrika Bambaataa',
+    'Public Enemy', 
+    'Run DMC', 
+    'The Fugees',
+    'The Roots',
+    'Common',
+    'People Under the Stairs',
+    'Beastie Boys',
+    'X Clan',
+    'Jungle Brothers',
+    'The Pharcyde',
+    'Dead Prez',
+    'Digable Planets',
+    'A Tribe Called Quest',
+    'Souls of Mischief',
+    'The Coup',
+    'Del the Funky Homosapien',
+    'Wu-Tang Clan',
+    'Lupe Fiasco',
+    'Atmosphere',
+    'Brand Nubian',
+    'Cypress Hill',
+    '2Pac',
+    'Queen Latifah',
+    'Digital Underground',
+    'Boogie Down Productions'
+];
+
+module.exports = phrases;
